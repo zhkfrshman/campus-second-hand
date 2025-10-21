e6f88c1f620b90a27e4106e3c1fab0a1d29d48fa
YiMing Wang <u202315500@hust.edu.cn>
2025-10-21 12:46:22 +0800

    创建spring boot项目

---
+++ Used_trading_platform/src/main/java/com/softwareengineering/team/used_trading_platform/UsedTradingPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.softwareengineering.team.used_trading_platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsedTradingPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsedTradingPlatformApplication.class, args);
+	}
+
+}
